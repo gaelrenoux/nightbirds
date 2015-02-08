77cf52dfe4e151455c3765fb375a71e750016326
gaelrenoux <gael.renoux@gmail.com>
2015-02-08 19:16:35 +0100

    Photograph now work on witnessing only

--- rules/src/fr/renoux/nightbirds/rules/cardtypes/Photograph.scala
+++ rules/src/fr/renoux/nightbirds/rules/cardtypes/Photograph.scala
@@ -14,26 +14,29 @@ import fr.renoux.nightbirds.rules.state.LeftNeighbour
 object PhotographType extends CardType(Legal)
 class Photograph(f: Family) extends Card(f)(PhotographType) with WithoutTarget {
 
-  override def activate(gs : GameState) = {
-    
-    val leftGuyTapped = position.get.left.map(_.get).map(_.tapped).getOrElse(false)
-    if (leftGuyTapped) {
+  override def activate() = {
+    for (i <- 0 until futureWitnessEffects) {
       store(Cash(Rules.PhotographEarnings))
     }
-    
-    val rightGuyTapped = position.get.right.map(_.get).map(_.tapped).getOrElse(false)
-    if (rightGuyTapped) {
-      store(Cash(Rules.PhotographEarnings))
-    }
+    futureWitnessEffects = 0
   }
 
   override val hasWitnessEffect = true
   
+  private var futureWitnessEffects = 0
+  
   override def witness(origin: Card) = {
     if (tapped) {
       store(Cash(Rules.PhotographEarnings))
+    } else {
+      futureWitnessEffects = futureWitnessEffects + 1
     }
     /* do nothing if not tapped, will happen when activated */
+  }
+  
+  override def sleep() = {
+    futureWitnessEffects = 0
+    super.sleep()
   }
 
 }
